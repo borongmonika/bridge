f80c5a5f9bc4a5bc3ea09f3ad196a54b005e8e84
Borong <15100377989@163.com>
2018-09-05 18:16:21 +0800

    轮播图

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,19 @@
 import styles from './home.css';
+import { Carousel } from 'antd';
+
 
 export default function() {
   return (
-    <div className={styles.normal}>
-      <h1>Page home</h1>
+    <div className={styles.normal} >
+      <Carousel autoplay effect="fade">
+    <div><h3><img src={require('../assets/tupian1.jpg')} style={{margin:'auto'}}/></h3></div>
+    <div><h3><img src={require('../assets/tupian2.jpg')} style={{margin:'auto'}}/></h3></div>
+    <div><h3><img src={require('../assets/tupian3.jpg')} style={{margin:'auto'}}/></h3></div>
+    <div><h3><img src={require('../assets/tupian4.jpg')} style={{margin:'auto'}} /></h3></div>
+  </Carousel>
+  
+
+
     </div>
   );
 }
